3bfb4a8f5196c8d128eedd3350a81642d346c77c
skellyska <97416175+skellyska@users.noreply.github.com>
2022-05-03 19:25:48 -0700

    Create outdoorParkConcertExample.py
    code

---
+++ outdoorParkConcertExample.py
@@ -0,0 +1,26 @@
+"""
+This example code creates a 2d list (2d matrix) that can store seating.
+The matrix is populated with a since all seats are available
+"""
+
+# our test matrix has 4 rows and 10 columns
+n_row = 4
+n_col = 10
+
+# available seat
+available_seat = 'a'
+
+# create some available seating
+seating = []
+for r in range(n_row):
+    row = []
+    for c in range(n_col):
+        row.append(available_seat)
+    seating.append(row)
+
+# print available seating
+for r in range(n_row):
+    print(r+1, end="\t")
+    for c in range(n_col):
+        print(seating[r][c], end=" ")
+    print()
