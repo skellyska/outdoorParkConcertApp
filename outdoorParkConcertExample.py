4eafa6bc07a1ab35c3bed2375e0939344569bcd6
skellyska <97416175+skellyska@users.noreply.github.com>
2022-05-04 08:12:39 -0700

    Update outdoorParkConcertExample.py
    added more direction and mapped out flow

--- outdoorParkConcertExample.py
+++ outdoorParkConcertExample.py
@@ -4,9 +4,49 @@ The matrix is populated with a since all seats are available
 
 resources:
 https://github.com/mylescruz/Tiny-Concert/blob/master/Tiny_Concert/env/app.py
+
+Menu system for guests to:
+[V]iew/display available seating
+An available seat is indicated with a "a" (lower case a)
+An already occupied seat is indicated with a "X" (capital x)
+There must be 2 social distancing seats (available seats) between each occupied seat on a row. 
+> 1 row distance between each row.  Bulk tickets that are purchased can sit next to each other.
+Provide 3 types of seating
+Front Seat with price $80.  Rows 0 - 4
+Middle Seat with price $50.  Rows 5-10
+Back Seat with price $25.  Rows 11-19
+[B]uy/purchase a ticket and provide receipt with  state tax of 7.25% including an additional mandatory mask fee of $5.00
+When a purchase is made, ask the user for their name & email address
+[S]earch by name will display the tickets purchased by a user with a specific name.
+[D]isplay all purchases.  Prints all the purchases made and shows the total amount of income/money that the venue has made.
+[Q]uit
+Tickets can be purchased 1 at a time or in bulk.  
+> For example, a user can ask to buy 15 tickets and they should be given pricing and a group of 15 tickets available for the type of ticket they wish to buy (Front, Middle, or Back)
+Seats should be labelled by rows and columns in a rectangular format
+Rows are indicated with numbers
+Columns are indicated with letters
+Your software should provide 520 seats (20 x 26) as shown in the images below.
+Your software saves data seating and purchase data into a .json  file as transactions are completed. 
+> When the application restarts (is quit and ran again by the user), the last saved information is loaded/restored.  So, seating and purchase information is not lost.
 """
 
-# our test matrix has 4 rows and 10 columns
+#FLOW
+#login and creating json
+#display seats and availability
+#load seat data for each row as arrays that store a seat's value as empty or taken
+
+
+def storeusers(form):
+    # gives data to all session variables
+    session['name'] = form.name.data
+    session['lastname'] = form.lastname.data
+    session['emailaddress'] = form.emailaddress.data
+    session['password'] = form.password.data
+
+
+
+
+
 n_row = 20
 n_col = 26
 
